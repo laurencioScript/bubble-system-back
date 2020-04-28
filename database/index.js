11b6eb490c7b328be4ec837fb3fc5e6a839999c1
Gabriel Laurencio <laurencio.arkauss@gmail.com>
2020-04-27 23:39:30 -0300

    config database

--- database/index.js
+++ database/index.js
@@ -1,9 +1,12 @@
-const Client = require("pg").Client;
+const Pool = require("pg").Pool;
 const connect = () => {
-  return new Client({
-    connectionString: "postgres://kdnbakozvszrvp:778ef3c70d200104b95e89d58ba61f0216ba79ad8c3418cdb67027fb156bfa7f@ec2-18-206-84-251.compute-1.amazonaws.com:5432/df2qkb2n05ro5a",
-    ssl: true
-  })
+  return new Pool({
+    user: "kdnbakozvszrvp",
+    host: "ec2-18-206-84-251.compute-1.amazonaws.com",
+    database: "df2qkb2n05ro5a",
+    password: "778ef3c70d200104b95e89d58ba61f0216ba79ad8c3418cdb67027fb156bfa7f",
+    port: 5432,
+  });
 };
 
 
