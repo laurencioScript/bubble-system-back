1064401a509195e83d7b6030168ffb477f3258a3
Gabriel Laurencio <laurencio.arkauss@gmail.com>
2020-04-27 22:56:59 -0300

    config database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const Client = require("pg").Client;
 const connect = () => {
   return new Client({
-    connectionString: "postgres://kdnbakozvszrvp:778ef3c70d200104b95e89d58ba61f0216ba79ad8c3418cdb67027fb156bfa7f@ec2-18-206-84-251.compute-1.amazonaws.com:5432/df2qkb2n05ro5a",
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   })
 };
