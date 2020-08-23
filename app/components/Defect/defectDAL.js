535724bcac1c34da42ff14dc9893a3c76aff6f80
laurencioScript <laurencio.arkauss@gmail.com>
2020-08-22 22:08:59 -0300

    fix : fixing a bug in route put defect

--- app/components/Defect/defectDAL.js
+++ app/components/Defect/defectDAL.js
@@ -1,106 +1,81 @@
-const {connect} = require('./../../../database');
-const uuidV4 = require('uuid/v4');
+const { connect } = require("./../../../database");
+const uuidV4 = require("uuid/v4");
 
-const createDefect =  async (defect) => {
+const createDefect = async (defect) => {
   const client = connect();
-  try{
-    
-    return  await client.query(`insert into defect  values 
+  try {
+    return await client.query(`insert into defect  values 
          ('${defect.id}','${defect.name}') `);
-
-  }
-  catch(error){
-    throw error;   
-  }
-  finally{
-    
+  } catch (error) {
+    throw error;
+  } finally {
     client.end();
   }
-
-}
-
+};
 
 const getDefects = async (values) => {
   const client = connect();
-  try{
-
-    let query = 'select * from defect ';
-    if(values.name ){
-      query += 'where ';
-      query += values.name ? `defect_name like '%${values.name}%' ` :"";
+  try {
+    let query = "select * from defect ";
+    if (values.name) {
+      query += "where ";
+      query += values.name ? `defect_name like '%${values.name}%' ` : "";
     }
 
     query += `order by defect_name desc limit '${values.limit}' offset '${values.offset}'`;
 
     return await client.query(query);
-    
-  }
-  catch(error){
-    throw error;   
-  }
-  finally{
-    
+  } catch (error) {
+    throw error;
+  } finally {
     client.end();
   }
-}
+};
 
 const getDefect = async (defectId) => {
   const client = connect();
-  try{
-    
-    return await client.query(`select * from defect where id_defect = '${defectId}'`);
-   
-  }
-  catch(error){
-    throw error;   
-  }
-  finally{
-    
+  try {
+    return await client.query(
+      `select * from defect where id_defect = '${defectId}'`
+    );
+  } catch (error) {
+    throw error;
+  } finally {
     client.end();
   }
-}
-
+};
 
 const updateDefect = async (defect) => {
   const defectExist = await getDefect(defect.id);
   const client = connect();
-  try{
-
+  try {
     return await client.query(`UPDATE defect 
-    SET defect_name = '${defect.defect ? defect.defect : defectExist.defect }'
+    SET defect_name = '${defect.name ? defect.name : defectExist.name}'
     where id_defect = '${defect.id}' `);
-    
-    
-  
-  }
-  catch(error){
-    throw error;   
-  }
-  finally{
-    
+  } catch (error) {
+    throw error;
+  } finally {
     client.end();
   }
-  
-}
-
+};
 
 const deleteDefect = async (defectId) => {
   const client = connect();
-  try{
-
-    return await client.query(`DELETE FROM defect WHERE id_defect = '${defectId}' `);
-    
-    
-
-  }
-  catch(error){
-    throw error;   
-  }
-  finally{
-    
+  try {
+    return await client.query(
+      `DELETE FROM defect WHERE id_defect = '${defectId}' `
+    );
+  } catch (error) {
+    throw error;
+  } finally {
     client.end();
   }
-  
-}
+};
 
-module.exports = {createDefect,getDefect,getDefects,updateDefect,deleteDefect};
+module.exports = {
+  createDefect,
+  getDefect,
+  getDefects,
+  updateDefect,
+  deleteDefect,
+};
--- app/components/Unity/Unity.js
+++ app/components/Unity/Unity.js
@@ -1,224 +1,206 @@
-const express = require('express');
-const Unity = require('./unityDAL');
+const express = require("express");
+const Unity = require("./unityDAL");
 const router = express.Router();
-const JWT= require('./../../../jwt/authConfig');
-const Joi = require('joi');
-const { isUuid }  = require('uuidv4');
-const uuidV4 = require('uuid/v4');
+const JWT = require("./../../../jwt/authConfig");
+const Joi = require("joi");
+const { isUuid } = require("uuidv4");
+const uuidV4 = require("uuid/v4");
 
 router.use(JWT.getMiddleware);
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    JWT.hasPermissions(req, res, 1);
 
-    try {
+    const SchemaUser = Joi.object().keys({
+      name: Joi.string().min(1).max(30).lowercase(),
+      limit: Joi.number().integer().min(0),
+      offset: Joi.number().integer().min(0),
+    });
 
-        JWT.hasPermissions(req,res,1);
+    const resultValidate = Joi.validate(req.query, SchemaUser);
 
-        const SchemaUser = Joi.object().keys({
-            name: Joi.string().min(1).max(30).lowercase(),
-            limit: Joi.number().integer().min(0),
-            offset: Joi.number().integer().min(0)
-        });
-
-        const resultValidate = Joi.validate(req.query, SchemaUser);
-
-        if(resultValidate.error !== null){
-            return res.status(400).send({error:resultValidate.error.details[0].message });
-        }
-
-        const {value} = resultValidate;
-        
-        value.limit =  value.limit ? value.limit : 10;
-        value.offset =  value.offset ? value.offset : 0; 
-
-        const result = await Unity.getUnitys(value);
-
-        if(result.rowCount < 1){
-            return res.status(404).send({error:'Not found'});
-        }
-
-        return res.status(200).send({result:result.rows})
-        
-    } catch (error) {
-
-        console.log(error);
-        
-
-        if(error.detail){
-            return res.status(400).send({error:error.detail});
-        }
-        else{
-            return res.status(400).send({"error":"Has error"});
-        }
+    if (resultValidate.error !== null) {
+      return res
+        .status(400)
+        .send({ error: resultValidate.error.details[0].message });
     }
 
-    
+    const { value } = resultValidate;
+
+    value.limit = value.limit ? value.limit : 10;
+    value.offset = value.offset ? value.offset : 0;
+
+    const result = await Unity.getUnitys(value);
+
+    if (result.rowCount < 1) {
+      return res.status(404).send({ error: "Not found" });
+    }
+
+    return res.status(200).send({ result: result.rows });
+  } catch (error) {
+    console.log(error);
+
+    if (error.detail) {
+      return res.status(400).send({ error: error.detail });
+    } else {
+      return res.status(400).send({ error: "Has error" });
+    }
+  }
 }); // List All Unity
 
-router.post('/register', async (req, res) => {
-    
-    try {
-        
-        JWT.hasPermissions(req,res,1);
+router.post("/register", async (req, res) => {
+  try {
+    console.log(">>> req", req);
+    JWT.hasPermissions(req, res, 1);
 
-        const SchemaUser = Joi.object().keys({
-            name: Joi.string().min(3).max(30).lowercase().required()
-        });
-    
-        const resultValidate = Joi.validate(req.body, SchemaUser);
-        
-        if(resultValidate.error !== null){
-            return res.status(400).send({error:resultValidate.error.details[0].message });
-        }
-    
-        resultValidate.value.id = uuidV4();
+    const SchemaUser = Joi.object().keys({
+      name: Joi.string().min(3).max(30).lowercase().required(),
+    });
+    console.log(">>> req.body", req.body);
+    const resultValidate = Joi.validate(req.body, SchemaUser);
+    console.log(">>> resultValidate", resultValidate);
 
-        await Unity.createUnity(resultValidate.value);
-        
-        resultValidate.value.password = undefined;
-
-        return res.status(200).send(resultValidate.value);
-
-    } catch (error) {
-
-       
-        if(error.detail){
-            return res.status(400).send({error:error.detail});
-        }
-        else{
-            return res.status(400).send({"error":"Has error"});
-        }
-        
+    if (resultValidate.error !== null) {
+      return res
+        .status(400)
+        .send({ error: resultValidate.error.details[0].message });
     }
 
-});// Create Unity
+    resultValidate.value.id = uuidV4();
 
-router.get('/:id', async (req, res) => {
+    await Unity.createUnity(resultValidate.value);
 
-    try{
-        
-        JWT.hasPermissions(req,res,1);
+    resultValidate.value.password = undefined;
 
-        const SchemaUser = Joi.object().keys({ id: Joi.string().required() });
-    
-        const resultValidate = Joi.validate(req.params, SchemaUser);
-        
-        if(resultValidate.error !== null){
-            return res.status(400).send({error:resultValidate.error.details[0].message });
-        }
+    return res.status(200).send(resultValidate.value);
+  } catch (error) {
+    if (error.detail) {
+      return res.status(400).send({ error: error.detail });
+    } else {
+      return res.status(400).send({ error: "Has error" });
+    }
+  }
+}); // Create Unity
 
-        if(!isUuid(resultValidate.value.id)){
-            return res.status(400).send({error:"The id is not valid" });
-        }
+router.get("/:id", async (req, res) => {
+  try {
+    JWT.hasPermissions(req, res, 1);
 
-        const result = await Unity.getUnity(resultValidate.value.id);
+    const SchemaUser = Joi.object().keys({ id: Joi.string().required() });
 
-        if(result.rowCount < 1){
-            return res.status(404).send({error:'Not found'});
-        }
+    const resultValidate = Joi.validate(req.params, SchemaUser);
 
-        return res.status(200).send({result:result.rows});
+    if (resultValidate.error !== null) {
+      return res
+        .status(400)
+        .send({ error: resultValidate.error.details[0].message });
+    }
 
-    }   
-    catch (error) {
+    if (!isUuid(resultValidate.value.id)) {
+      return res.status(400).send({ error: "The id is not valid" });
+    }
 
-        console.log('LOG',error);
-        
-        if(error.detail){
-            return res.status(400).send({error:error.detail});
-        }
-        
-        return res.status(400).send({"error":"Has error"});
-    } 
+    const result = await Unity.getUnity(resultValidate.value.id);
 
+    if (result.rowCount < 1) {
+      return res.status(404).send({ error: "Not found" });
+    }
+
+    return res.status(200).send({ result: result.rows });
+  } catch (error) {
+    console.log("LOG", error);
+
+    if (error.detail) {
+      return res.status(400).send({ error: error.detail });
+    }
+
+    return res.status(400).send({ error: "Has error" });
+  }
 }); //List One Unity
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
+  try {
+    JWT.hasPermissions(req, res, 2);
 
-    try{
+    const SchemaUser = Joi.object().keys({
+      id: Joi.string().required(),
+      name: Joi.string().min(3).max(30).lowercase(),
+    });
 
-        JWT.hasPermissions(req,res,2);
+    const resultValidate = Joi.validate(
+      { id: req.params.id, ...req.body },
+      SchemaUser
+    );
 
-        const SchemaUser = Joi.object().keys({
-            id: Joi.string().required(),
-            name: Joi.string().min(3).max(30).lowercase()
-        });
-        
-        const resultValidate = Joi.validate({"id":req.params.id,...req.body}, SchemaUser);
-            
-        if(resultValidate.error !== null){
-            return res.status(400).send({error:resultValidate.error.details[0].message });
-        }
+    if (resultValidate.error !== null) {
+      return res
+        .status(400)
+        .send({ error: resultValidate.error.details[0].message });
+    }
 
-        if(!isUuid(resultValidate.value.id)){
-            return res.status(400).send({error:"The id is not valid" });
-        }
-        
-        const result = await Unity.updateUnity(resultValidate.value);
-    
-        if(result.rowCount < 1){
-            return res.status(404).send({error:'Not found'});
-        }
+    if (!isUuid(resultValidate.value.id)) {
+      return res.status(400).send({ error: "The id is not valid" });
+    }
 
-        return res.status(200).send({result:resultValidate.value})
-    }   
-    catch (error) {
+    const result = await Unity.updateUnity(resultValidate.value);
 
-        console.log('LOG',error);
-        
-        if(error.detail){
-            return res.status(400).send({error:error.detail});
-        }
-        
-        return res.status(400).send({"error":"Has error"});
-    }  
-    
-    const result = await Unity.updateUnity({"id":req.params.id,...req.body});
+    if (result.rowCount < 1) {
+      return res.status(404).send({ error: "Not found" });
+    }
 
-    return (!result.error) ? res.status(200).send({"result":"row update"}) : res.status(400).send({"result":result});
+    return res.status(200).send({ result: resultValidate.value });
+  } catch (error) {
+    console.log("LOG", error);
 
+    if (error.detail) {
+      return res.status(400).send({ error: error.detail });
+    }
+
+    return res.status(400).send({ error: "Has error" });
+  }
+
+  const result = await Unity.updateUnity({ id: req.params.id, ...req.body });
+
+  return !result.error
+    ? res.status(200).send({ result: "row update" })
+    : res.status(400).send({ result: result });
 }); //Editar
 
-router.delete('/:id', async (req, res) => {
-    
-    try{
+router.delete("/:id", async (req, res) => {
+  try {
+    JWT.hasPermissions(req, res, 2);
 
-        JWT.hasPermissions(req,res,2);
-        
-        const SchemaUser = Joi.object().keys({ id: Joi.string().required() });
-        
-        const resultValidate = Joi.validate(req.params, SchemaUser);
-            
-        if(resultValidate.error !== null){
-            return res.status(400).send({error:resultValidate.error.details[0].message });
-        }
-    
-        if(!isUuid(resultValidate.value.id)){
-            return res.status(400).send({error:"The id is not valid" });
-        }
-        
-        const result = await Unity.deleteUnity(resultValidate.value.id);
-    
-        if(result.rowCount < 1){
-            return res.status(404).send({error:'Not found'});
-        }
+    const SchemaUser = Joi.object().keys({ id: Joi.string().required() });
 
-        return res.status(200).send({result:resultValidate.value})
+    const resultValidate = Joi.validate(req.params, SchemaUser);
 
-    }   
-    catch (error) {
+    if (resultValidate.error !== null) {
+      return res
+        .status(400)
+        .send({ error: resultValidate.error.details[0].message });
+    }
 
-        console.log('LOG',error);
-        
-        if(error.detail){
-            return res.status(400).send({error:error.detail});
-        }
-        
-        return res.status(400).send({"error":"Has error"});
-    } 
-    
+    if (!isUuid(resultValidate.value.id)) {
+      return res.status(400).send({ error: "The id is not valid" });
+    }
+
+    const result = await Unity.deleteUnity(resultValidate.value.id);
+
+    if (result.rowCount < 1) {
+      return res.status(404).send({ error: "Not found" });
+    }
+
+    return res.status(200).send({ result: resultValidate.value });
+  } catch (error) {
+    console.log("LOG", error);
+
+    if (error.detail) {
+      return res.status(400).send({ error: error.detail });
+    }
+
+    return res.status(400).send({ error: "Has error" });
+  }
 }); //Deletar
 
-
-module.exports = app => app.use('/unity',router);
-
+module.exports = (app) => app.use("/unity", router);
